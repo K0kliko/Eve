c9b98127568e2f559b720c7a470c2ed206bdfa53
Kokliko <131816378+K0kliko@users.noreply.github.com>
2023-04-30 02:57:29 +0200

    Update Eve.bat
    -add to choice to yes no error cls

--- Eve.bat
+++ Eve.bat
@@ -1,9 +1,29 @@
 @echo off
 color f
-title Eve.bat
+title Eve
 cls
-echo Do you really want to put your PC to sleep (y/n)
-date /t
-time /t
-pause
-shutdown /h
+
+:choice
+set /p choix=Do you really want to put your PC to sleep (y/n)
+
+    if /i "%choix%"=="y" (goto :yes)
+    if /i "%choix%"=="n" (goto :no)
+    goto :ERROR
+
+:yes
+
+    cls
+    echo Your PC will go to sleep.
+    pause
+    shutdown /h
+
+:no
+    cls
+    echo Your PC will not sleep.
+    pause
+
+:ERROR
+    echo ERROR : Please choose between y (yes) or n (no).
+    pause
+    cls
+    goto :choice
